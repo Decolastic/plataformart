d6b5a725c7ef885610df20fe4cd825bb3a1c123d
Decolastic <110244042+Decolastic@users.noreply.github.com>
2023-04-13 19:34:03 -0600

    Instalacion correcta de MUI MATERIAL

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Grid } from "@mui/material";
 import type { V2_MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { useState } from "react";
@@ -17,8 +18,8 @@ export default function Index() {
   const user = useOptionalUser();
   
   return (
+
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-         
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
